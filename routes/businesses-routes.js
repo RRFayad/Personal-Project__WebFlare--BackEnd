e98056c4a072092230f495bdfa598a4aecdd4599
RRFayad <renan.r.fayad@gmail.com>
2023-09-05 09:55:17 -0300

    Businesses validator v0.1

--- routes/businesses-routes.js
+++ routes/businesses-routes.js
@@ -8,8 +8,36 @@ const router = express.Router();
 router.get('/', businessControllers.getAllBusinesses);
 router.get('/user/:uid', businessControllers.getBusinessesByUserId);
 router.get('/:bid', businessControllers.getBusinessById);
-router.post('/', businessControllers.createBusiness);
-router.patch('/:bid', businessControllers.updateBusinessById);
+router.post(
+  '/',
+  [
+    check('title').isLength({ min: 3 }),
+    check('type').notEmpty(),
+    check('niche').notEmpty(),
+    check('age').notEmpty().isNumeric,
+    check('monthlyRevenue').notEmpty().isNumeric,
+    check('monthlyProfit').notEmpty().isNumeric,
+    check('askingPrice').notEmpty().isNumeric,
+    check('description').isLength({ min: 6 }),
+    check('ownerId').notEmpty(),
+  ],
+  businessControllers.createBusiness,
+);
+router.patch(
+  '/:bid',
+  [
+    check('title').isLength({ min: 3 }),
+    check('type').notEmpty(),
+    check('niche').notEmpty(),
+    check('age').notEmpty().isNumeric,
+    check('monthlyRevenue').notEmpty().isNumeric,
+    check('monthlyProfit').notEmpty().isNumeric,
+    check('askingPrice').notEmpty().isNumeric,
+    check('description').isLength({ min: 6 }),
+    check('ownerId').notEmpty(),
+  ],
+  businessControllers.updateBusinessById,
+);
 router.delete('/:bid', businessControllers.deleteBusinessById);
 
 module.exports = router;
