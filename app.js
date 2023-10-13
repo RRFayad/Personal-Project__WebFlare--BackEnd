170d8c4ac6caa3ecec8ca22c4f444509f944b0d4
RRFayad <renan.r.fayad@gmail.com>
2023-10-13 17:51:35 -0300

    env PORT for depolyment updated

--- app.js
+++ app.js
@@ -39,6 +39,6 @@ mongoose
     `mongodb+srv://renanrfayad:${process.env.MONGO_PROJECT_PASSWORD}@webflare-cluster.qhiblj4.mongodb.net/`,
   )
   .then(() => {
-    app.listen(5000);
+    app.listen(process.env.PORT || 5000);
   })
   .catch(err => console.log(err));
